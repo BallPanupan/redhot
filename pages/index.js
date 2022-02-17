3f1a6dc7e50e5d3d09029c725b894bcd495e465a
Ball Panupan <Ball@reverseads.com>
2022-02-17 15:32:10 +0700

    install react-bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+
+
 export default function Home() {
-    return (
-        <div className="container">
-            <h1>Hello </h1>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1>Hello </h1>
+    </div>
+  );
 }
