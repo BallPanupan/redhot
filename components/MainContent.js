f6640a4992f6d5d4af4f3adcc20a019f75132ec4
Ball Panupan <Ball@reverseads.com>
2022-02-21 09:26:51 +0700

    update MainContent

---
+++ components/MainContent.js
@@ -0,0 +1,10 @@
+function MainContent() {
+    return (
+        <div className="main-content">
+            <div className="left">1</div>
+            <div className="right">2</div>
+        </div>
+    );
+  }
+  
+  export default MainContent;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import { Container} from "react-bootstrap";
-import NavbarMenu from "../components/NavbarMenu";
+import MainContent from "../components/MainContent";
 
 export default function Home() {
   return (
     <>
 
       <Container>
+        <MainContent/>
         <div className="content-header">
           <h1>Hello Kal</h1>
         </div>
